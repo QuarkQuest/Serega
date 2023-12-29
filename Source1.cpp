9d9def2fa44d5b65488147262237dcab16e9ab1b
QuarkQuest <153836688+QuarkQuest@users.noreply.github.com>
2023-12-29 19:15:13 +0700

    добавил коменты

--- Source1.cpp
+++ Source1.cpp
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-int Max(int a, int b) {
+int Max(int a, int b) {//функция определения максимума
 	if (a > b) {
 		return a;
 	}
@@ -10,7 +10,7 @@ int Max(int a, int b) {
 		return b;
 	}
 }
-void output(int** a, int n, int m) {
+void output(int** a, int n, int m) {//функция вывода матрицы
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			printf("%4d", a[i][j]);
@@ -19,7 +19,7 @@ void output(int** a, int n, int m) {
 	}
 	printf("\n");
 }
-int randomized(int** a,int n,int m) {
+int randomized(int** a,int n,int m) {//Функция рандомного заполнения матрицы
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			a[i][j] = 1 + rand() % 100;
@@ -27,7 +27,15 @@ int randomized(int** a,int n,int m) {
 	}
 	return **a;
 }
-int transport(int n,int m,int **a) {
+int transport(int n,int m,int **a) {//функция транспонирования матрицы
+	/*
+	Аллгоритм очень простой и построен на знаний неизменой дисгонали то есть 
+	все что мы делаем меняем i,j элемент на j,i элемент 
+	|1|2|3|		 |1|4|7|
+	|4|5|6| -->  |2|5|8|
+	|7|8|9|		 |3|6|9|
+	2 на 4, 3 на 7, 8 на 6
+	*/
 	int max = Max(n, m);
 	if (n != m) {
 		for (int i = 0; i < max; i++) {
@@ -54,6 +62,13 @@ int main() {
 	int n,m;
 	scanf("%d", &n);
 	scanf("%d", &m);
+	/*
+	Данный алгоритм идеально работает для квадратный матриц,
+	но плохо для прямогуольной поэтому всегда создается квадратная матрица 
+	даже если матрица прямоугольная (часть вводимая пользователем рандомизируется
+	а не использованная часть зануляется с помощью функции calloc)
+	colloc - тот же malloc только изначально инициализирует нули вместо ничего
+	*/
 	int max = Max(n, m);
 	int** a;
 	a = (int**)calloc(max,sizeof(int*));
