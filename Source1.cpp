f8e455aaac572066584be70f3cfba47341ee44fc
QuarkQuest <153836688+QuarkQuest@users.noreply.github.com>
2024-05-09 16:14:51 +0700

    new

--- Source1.cpp
+++ Source1.cpp
@@ -1,88 +1,68 @@
 #pragma warning (disable : 4996)
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int Max(int a, int b) {//функция определения максимума
-	if (a > b) {
-		return a;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef unsigned int UI;
+UI InputBV(char s[], int n) {
+	UI x = 0;
+	int i = 0;
+	for (i = 0; i < n; i++) {
+		x <<= 1;
+		if (s[i] == '1')x++;
 	}
-	else {
-		return b;
+	return x;
+}
+int Wieght(UI x, int n) {
+	int w, i;
+	UI mask = 1;
+	for (w = 0, i = 0; i < n; i++, mask <<= 1) {
+		if (x & mask)w++;
+	}
+	return w;
+}
+int SetUp1(UI x,int k) {
+	UI mask = 1;
+	mask <<= k;
+	x |= mask;
+	return x;
+}
+UI Invert(UI x, int k) {
+	UI mask = 1;
+	mask <<= k;
+	x ^= mask;
+	return x;
+}
+void OutputBV(UI x, int n) {
+	UI mask = 1; int i;
+	mask <<= n - 1;
+	for (i = 0; i < n; i++, mask >>= 1) {
+		if (x & mask)printf("1");
+		else printf("0");
 	}
 }
-void output(int** a, int n, int m) {//функция вывода матрицы
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%4d", a[i][j]);
-		}
-		printf("\n");
-	}
+void io(UI num,int n) {
+	for (int i = n; i >= 0; --i)
+		printf("%d", ((num >> i) & 1));
 	printf("\n");
 }
-int randomized(int** a,int n,int m) {//Функция рандомного заполнения матрицы
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			a[i][j] = 1 + rand() % 100;
-		}
-	}
-	return **a;
-}
-int transport(int n,int m,int **a) {//функция транспонирования матрицы
-/*
-Аллгоритм очень простой и построен на знаний неизменой дисгонали то есть 
-все что мы делаем меняем i,j элемент на j,i элемент 
-|1|2|3|		 |1|4|7|
-|4|5|6| -->  |2|5|8|
-|7|8|9|		 |3|6|9|
-2 на 4, 3 на 7, 8 на 6
-*/
-	int max = Max(n, m);
-	if (n != m) {
-		for (int i = 0; i < max; i++) {
-			for (int j = i + 1; j < max; j++) {
-				int temp = a[i][j];
-				a[i][j] = a[j][i];
-				a[j][i] = temp;
-			}
-		}
-	}
-	else {
-		for (int i = 0; i < max; i++) {
-			for (int j = i + 1; j < max; j++) {
-				int temp = a[i][j];
-				a[i][j] = a[j][i];
-				a[j][i] = temp;
-			}
-		}
-
-	}
-	return **a;
-}
 int main() {
-	int n,m;
-	scanf("%d", &n);
-	scanf("%d", &m);
-	/*
-	Данный алгоритм идеально работает для квадратный матриц,
-	но плохо для прямогуольной поэтому всегда создается квадратная матрица 
-	даже если матрица прямоугольная (часть вводимая пользователем рандомизируется
-	а не использованная часть зануляется с помощью функции calloc)
-	colloc - тот же malloc только изначально инициализирует нули вместо ничего
-	*/
-	int max = Max(n, m);
-	int** a;
-	a = (int**)calloc(max,sizeof(int*));
-	for (int i = 0; i < max; i++) {
-		a[i] = (int*)calloc(max,sizeof(int));
-	}
-	srand(time(NULL));
-	randomized(a, n, m);
-	output(a, n, m);
-	transport(n, m, a);
-	output(a, m, n);
+	char s[99];
+	int k;
+	UI x;
+	printf("input v: ");
+	scanf("%s", &s);
+	int n = strlen(s);
+	printf("input k: ");
+	scanf("%d", &k);
+	printf("\n\n");
+	x = InputBV(s, n);
+	io(x, n - 1);
+	x = SetUp1(x, k);
+	io(x, n - 1);
+	x = Invert(x, k+1);
+	io(x, n - 1);
 
-	for (int i = 0; i < max; i++) {
-		free(a[i]);
-	}
-	free(a);
+	printf("insert %d bit, and invert %d bit: ",k,k+1);
+	io(x,n-1);
+	printf("Weight V = %d", Wieght(x, n));
 }
